b76746ee87e2d6d17d470605e94140d3699be375
Ketan1995-dev <s.ketan95@gmail.com>
2024-11-06 22:53:07 +0530

    Completed ConnectWise Assignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+interface IToDo{
+  id:number;
+  todo:string;
+  completed:boolean;
+  userId:number;
+}
+
+const pageSize = 10;
+
 function App() {
+  const [todos,setTodos] = useState<IToDo[]>([]);
+  const [total,setTotal] = useState<number>(0);
+  const [pageNumber,setPageNumber] = useState<number>(1);
+  const totalPages = Math.ceil(total/pageSize);
+
+  const pages = Array.from({length:totalPages},(_,i)=>i+1);
+
+  const onPageClick = (currentPage:number)=>{
+    setPageNumber(currentPage);
+  }
+
+  const onPreviousButtonClick = ()=>{
+    setPageNumber(pageNumber-1);
+  }
+
+  const onNextButtonClick = ()=>{
+    setPageNumber(pageNumber+1);
+  }  
+
+  useEffect(()=>{
+
+    const itemsToSkip = (pageNumber-1)*pageSize;
+
+    fetch('https://dummyjson.com/todos?limit='+pageSize+'&skip='+itemsToSkip)
+    .then(res => res.json())
+    .then((data)=>{
+      console.log(data);
+      setTodos(data.todos);
+      setTotal(data.total);
+      
+    });
+
+  },[pageNumber]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>Task ID</th>
+            <th>Todo</th>
+            <th>Completed</th>
+            <th>User ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            todos.map((todo)=>{
+              return <tr>
+              <td>{todo.id}</td>
+              <td>{todo.todo}</td>
+              <td>{todo.completed}</td>
+              <td>{todo.userId}</td>
+            </tr>
+            })
+          }
+        </tbody>
+      </table>
+      <div style={{textAlign:'left'}}>
+        { pageNumber>1 && <input type="button" value="<<" onClick={()=>onPageClick(1)} />}
+        { pageNumber>1 && <input type="button" value="<" onClick={onPreviousButtonClick} />}
+
+        {
+            pages.map((p)=>{
+              return <input type="button" value={p} onClick={()=>onPageClick(p)} />
+            })
+        }
+
+
+        { pageNumber<totalPages && <input type="button" value=">" onClick={onNextButtonClick} />}
+        { pageNumber<totalPages && <input type="button" value=">>" onClick={()=>onPageClick(totalPages)} /> }
+
+      </div>
     </div>
   );
+
+
 }
 
 export default App;
